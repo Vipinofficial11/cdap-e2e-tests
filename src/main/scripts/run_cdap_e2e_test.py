32bbc97a5b2256ab7f13cbf289e907201bb0d4d7
vipinbhatt <vipin.bhatt@cloudsufi.com>
2023-12-18 11:46:53 +0530

    Get folder version

--- src/main/scripts/run_cdap_e2e_test.py
+++ src/main/scripts/run_cdap_e2e_test.py
@@ -21,6 +21,8 @@ import shutil
 import argparse
 import urllib.request
 import yaml
+import re
+
 
 def run_shell_command(cmd):
     process = subprocess.run(cmd.split(" "), stderr=subprocess.PIPE)
@@ -28,10 +30,24 @@ def run_shell_command(cmd):
         print("Process completed with error: ", process.stderr)
     assert process.returncode == 0
 
+
+def get_sandbox_version(directory_path):
+    pattern = re.compile(r"cdap-sandbox-(\d+\.\d+\.\d+)-SNAPSHOT.zip")
+    # Iterate over files and folders in the directory
+    for entry in os.listdir(directory_path):
+        entry_path = os.path.join(directory_path, entry)
+    # Check if it's a directory and matches the pattern
+    if os.path.isdir(entry_path) and pattern.match(entry):
+        version_match = pattern.search(entry)
+        if version_match:
+            version = version_match.group(1)
+            return version
+
+
 # Parse command line optional arguments
-parser=argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 parser.add_argument('--testRunner', help='TestRunner class to execute tests')
-args=parser.parse_args()
+args = parser.parse_args()
 
 print("Building CDAP Sandbox")
 os.chdir("./plugin")
@@ -42,7 +58,7 @@ my_env["MAVEN_OPTS"] = "-Xmx1024m -XX:MaxPermSize=128m"
 my_env["_JAVA_OPTIONS"] = "-Xmx32G"
 my_env["HYDRATOR_PLUGINS"] = "../hydrator-plugins"
 
-#Building the plugins
+# Building the plugins
 os.chdir("..")
 hydrator_repository_url = "https://github.com/cdapio/hydrator-plugins.git"
 subprocess.run(["git", "clone", hydrator_repository_url])
@@ -51,13 +67,14 @@ run_shell_command("git submodule update --init --recursive --remote")
 run_shell_command("mvn clean install -DskipTests")
 os.chdir("../plugin")
 
-
 print(os.environ)
-#Building the sandbox
+# Building the sandbox
 run_shell_command('mvn clean package -pl cdap-standalone,cdap-app-templates/cdap-etl -am -amd -DskipTests -P '
                   f'templates,dist,release,unit-tests -Dadditional.artifacts.dir=../../hydrator-plugins/')
 os.chdir("./cdap-standalone/target")
-sandbox_zip = "cdap-sandbox-6.11.0-SNAPSHOT.zip"
+print("Printing the folders: " + subprocess.run("ls"))
+sandbox_version = get_sandbox_version(os.getcwd())
+sandbox_zip = f"cdap-sandbox-{sandbox_version}-SNAPSHOT.zip"
 print("cwd before extracting :", os.getcwd())
 with zipfile.ZipFile(sandbox_zip, 'r') as z:
     z.extractall(os.getcwd())
@@ -91,7 +108,8 @@ finally:
 
 cwd = os.getcwd()
 print("Copying sandbox logs to e2e-debug")
-shutil.copytree(cwd+"/sandbox/"+sandbox_dir+"/data/logs", cwd+"/plugin/target/cdap-e2e-tests/e2e-debug/sandbox/data/logs")
-shutil.copytree(cwd+"/sandbox/"+sandbox_dir+"/logs", cwd+"/plugin/cdap-e2e-tests/target/e2e-debug/sandbox/logs")
+shutil.copytree(cwd + "/sandbox/" + sandbox_dir + "/data/logs",
+                cwd + "/plugin/target/cdap-e2e-tests/e2e-debug/sandbox/data/logs")
+shutil.copytree(cwd + "/sandbox/" + sandbox_dir + "/logs", cwd + "/plugin/cdap-e2e-tests/target/e2e-debug/sandbox/logs")
 if assertion_error != None:
     raise assertion_error
