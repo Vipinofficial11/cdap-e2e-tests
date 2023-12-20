d00273e2062861ec2905b0ce4fecb8f59cc47929
vipinbhatt <vipin.bhatt@cloudsufi.com>
2023-12-20 20:50:36 +0530

    Get version from the folders

--- src/main/scripts/run_cdap_e2e_test.py
+++ src/main/scripts/run_cdap_e2e_test.py
@@ -80,70 +80,70 @@ print(args.cdapBranch)
 if args.cdapBranch:
     cdap_branch = args.cdapBranch
 print("CDAP BRANCH against which PR is raised: ", cdap_branch)
-# cdap_build_branch = cdap_branch
-# cdap_build_repository_url = "https://github.com/cdapio/cdap-build.git"
-# subprocess.run(["git", "clone", "-b", cdap_build_branch, cdap_build_repository_url])
-#
-# # .gitmodules file path and hydrator submodule path.
-# gitmodules_path = "./cdap-build/.gitmodules"
-# submodule_path = "app-artifacts/hydrator-plugins"
-# branch = get_hydrator_branch(gitmodules_path, submodule_path)
-#
-# if branch is not None:
-#     print(f"The branch of submodule '{submodule_path}' is: {branch}")
-# else:
-#     print(f"Submodule '{submodule_path}' not found in .gitmodules file.")
-#
-# hydrator_repository_branch = branch
-# hydrator_repository_url = "https://github.com/cdapio/hydrator-plugins.git"
-# subprocess.run(["git", "clone", "-b", hydrator_repository_branch, hydrator_repository_url])
-# os.chdir("./hydrator-plugins")
-# run_shell_command("git submodule update --init --recursive --remote")
-# run_shell_command("mvn clean install -DskipTests")
-# os.chdir("../plugin")
-#
-# print(os.environ)
-# # Building the sandbox
-# run_shell_command('mvn clean package -pl cdap-standalone,cdap-app-templates/cdap-etl -am -amd -DskipTests -P '
-#                   f'templates,dist,release,unit-tests -Dadditional.artifacts.dir=../../hydrator-plugins/')
-# os.chdir("./cdap-standalone/target")
-# sandbox_version = get_sandbox_version(os.getcwd())
-# sandbox_zip = f"cdap-sandbox-{sandbox_version}-SNAPSHOT.zip"
-# print("cwd before extracting :", os.getcwd())
-# with zipfile.ZipFile(sandbox_zip, 'r') as z:
-#     z.extractall(os.getcwd())
-#
-# sandbox_dir = sandbox_zip.replace(".zip", "")
-# run_shell_command(f"chmod +x {sandbox_dir}/bin/cdap")
-# sandbox_start_cmd = f"{sandbox_dir}/bin/cdap sandbox restart"
-# process = subprocess.Popen(f"{sandbox_start_cmd}", shell=True, env=my_env)
-# process.communicate()
-# assert process.returncode == 0
-#
-# # Run e2e tests
-# print("Running e2e integration tests for cdap")
-#
-# testrunner_to_run = ""
-# if args.testRunner:
-#     testrunner_to_run = args.testRunner
-# os.chdir("../../..")
-# assertion_error = None
-# try:
-#     os.chdir("./plugin/cdap-e2e-tests")
-#     if testrunner_to_run:
-#         print("TestRunner to run : " + testrunner_to_run)
-#         run_shell_command(f"mvn verify -P e2e-tests -DTEST_RUNNER={testrunner_to_run}")
-#     else:
-#         run_shell_command(f"mvn verify -P e2e-test")
-# except AssertionError as e:
-#     assertion_error = e
-# finally:
-#     os.chdir("../..")
-#
-# cwd = os.getcwd()
-# print("Copying sandbox logs to e2e-debug")
-# shutil.copytree(cwd + "/sandbox/" + sandbox_dir + "/data/logs",
-#                 cwd + "/plugin/target/cdap-e2e-tests/e2e-debug/sandbox/data/logs")
-# shutil.copytree(cwd + "/sandbox/" + sandbox_dir + "/logs", cwd + "/plugin/cdap-e2e-tests/target/e2e-debug/sandbox/logs")
-# if assertion_error != None:
-#     raise assertion_error
+cdap_build_branch = cdap_branch
+cdap_build_repository_url = "https://github.com/cdapio/cdap-build.git"
+subprocess.run(["git", "clone", "-b", cdap_build_branch, cdap_build_repository_url])
+
+# .gitmodules file path and hydrator submodule path.
+gitmodules_path = "./cdap-build/.gitmodules"
+submodule_path = "app-artifacts/hydrator-plugins"
+branch = get_hydrator_branch(gitmodules_path, submodule_path)
+
+if branch is not None:
+    print(f"The branch of submodule '{submodule_path}' is: {branch}")
+else:
+    print(f"Submodule '{submodule_path}' not found in .gitmodules file.")
+
+hydrator_repository_branch = branch
+hydrator_repository_url = "https://github.com/cdapio/hydrator-plugins.git"
+subprocess.run(["git", "clone", "-b", hydrator_repository_branch, hydrator_repository_url])
+os.chdir("./hydrator-plugins")
+run_shell_command("git submodule update --init --recursive --remote")
+run_shell_command("mvn clean install -DskipTests")
+os.chdir("../plugin")
+
+print(os.environ)
+# Building the sandbox
+run_shell_command('mvn clean package -pl cdap-standalone,cdap-app-templates/cdap-etl -am -amd -DskipTests -P '
+                  f'templates,dist,release,unit-tests -Dadditional.artifacts.dir=../../hydrator-plugins/')
+os.chdir("./cdap-standalone/target")
+sandbox_version = get_sandbox_version(os.getcwd())
+sandbox_zip = f"cdap-sandbox-{sandbox_version}-SNAPSHOT.zip"
+print("cwd before extracting :", os.getcwd())
+with zipfile.ZipFile(sandbox_zip, 'r') as z:
+    z.extractall(os.getcwd())
+
+sandbox_dir = sandbox_zip.replace(".zip", "")
+run_shell_command(f"chmod +x {sandbox_dir}/bin/cdap")
+sandbox_start_cmd = f"{sandbox_dir}/bin/cdap sandbox restart"
+process = subprocess.Popen(f"{sandbox_start_cmd}", shell=True, env=my_env)
+process.communicate()
+assert process.returncode == 0
+
+# Run e2e tests
+print("Running e2e integration tests for cdap")
+
+testrunner_to_run = ""
+if args.testRunner:
+    testrunner_to_run = args.testRunner
+os.chdir("../../..")
+assertion_error = None
+try:
+    os.chdir("./plugin/cdap-e2e-tests")
+    if testrunner_to_run:
+        print("TestRunner to run : " + testrunner_to_run)
+        run_shell_command(f"mvn verify -P e2e-tests -DTEST_RUNNER={testrunner_to_run}")
+    else:
+        run_shell_command(f"mvn verify -P e2e-test")
+except AssertionError as e:
+    assertion_error = e
+finally:
+    os.chdir("../..")
+
+cwd = os.getcwd()
+print("Copying sandbox logs to e2e-debug")
+shutil.copytree(cwd + "/sandbox/" + sandbox_dir + "/data/logs",
+                cwd + "/plugin/target/cdap-e2e-tests/e2e-debug/sandbox/data/logs")
+shutil.copytree(cwd + "/sandbox/" + sandbox_dir + "/logs", cwd + "/plugin/cdap-e2e-tests/target/e2e-debug/sandbox/logs")
+if assertion_error != None:
+    raise assertion_error
